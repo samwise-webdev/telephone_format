6271f98ac29817cd3f6ebcf4e99f6339e85bdd68
Sam Burgett <samwise.burgett@outlook.com>
2024-05-28 11:43:37 -0400

    empty check function

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const userInput = document.getElementById("user-input");
+const userInputValue = document.getElementById("user-input").value;
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const resultsDiv = document.getElementById("results-div");
+
+function validateButtonFunctions() {
+    empty();
+}
+
+function empty() {
+    if (userInput.value.length <= 0) {
+        alert("Please input a value")
+    }
+}
